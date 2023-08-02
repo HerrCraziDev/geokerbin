4b4ce70e8f95c025bf2fff9d1378b7c99e0171bb
HerrCraziDev <herrcrazi@gmail.com>
2023-08-02 04:49:19 +0200

    Fix tiling issue where the top line of the atlas is empty

--- tools/Atlas.py
+++ tools/Atlas.py
@@ -1,6 +1,7 @@
 from typing import List
 import math
 from PIL import Image
+from PIL import PyAccess
 
 # from ZoomLevel import ZoomLevel
 
@@ -33,23 +34,40 @@ class Atlas:
         
         return output
     
-    def stitch(self, output_path):
+    def stitch(self, output_path, fix_top_rows = False):
         rows = math.ceil(self.height / self.tile_size)
         cols = math.ceil(self.width / self.tile_size)
 
         print(f"[Atlas] Stitching {self.tilecount} tiles to {cols}x{rows} atlas ({self.width}px x {self.height}px), tile size: {self.tile_size}")
         print(f"[Atlas] Creating canvas...")
-        full = Image.new('RGBA', (self.width, self.height), (0,0,0,0))
+        full = Image.new('RGBA', (self.width, self.height), (0,0,0))
 
-        print(f"Stitching atlas...")
+        print(f"[Atlas] Stitching atlas...")
         c = 1
         for x in range(0, cols):
             for y in range(0, rows):
-                print(f"[Atlas] Processing tile {c} of {self.tilecount} ({(c/self.tilecount)*100:.2f}%) - Tile ({x},{y})          ", end='\r')
-                with Image.open(self.tiles[x][y]) as tile:
-                    full.paste(tile, (x * self.tile_size, y * self.tile_size))
-                    tile.close()
-                    c += 1
+                print(f"[Atlas] Processing tile {c} of {self.tilecount} ({(c/self.tilecount)*100:.2f}%) - Tile ({x},{y}) at ({x * self.tile_size}, {y * self.tile_size})          ", end='\r')
+                tile = Image.open(self.tiles[x][y])
+                full.paste(tile, (x * self.tile_size, y * self.tile_size))
+                tile.close()
+                c += 1
+        print()
         
+        if fix_top_rows:
+            px = full.load()
+            self.fix_top_rows(px)
+        
+        print(f"[Atlas] Saving to {output_path}...", end='', flush=True)
         full.save(output_path, 'PNG')
-        print("\nFile saved to " + output_path)
+        print("OK")
+    
+    def fix_top_rows(self, full: PyAccess):
+        for i in range(0, int(self.height/self.tile_size)):
+            print(f"[Atlas] Fixing top row {i} of {int(self.height/self.tile_size)}     ", end='\r',flush=True)
+            for x in range(0, self.width):
+                y = i * self.tile_size
+                if i == 0:
+                    full[x,y] = full[x,y+1]
+                else:
+                    full[x,y] = ( int((full[x,y-1][0] + full[x,y+1][0]) / 2), int((full[x,y-1][1] + full[x,y+1][1]) / 2), int((full[x,y-1][2] + full[x,y+1][2]) / 2) )
+        return full
