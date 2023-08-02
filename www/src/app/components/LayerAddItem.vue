547eeba3174ca2fc3877a40de27d18cc350a2ba5
HerrCraziDev <herrcrazi@gmail.com>
2023-08-02 04:55:33 +0200

    "Add Layer" button

---
+++ www/src/app/components/LayerAddItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="item-frame">
+        <Button><Icon>library_add</Icon>Add Layer</Button>
+    </div>
+</template>
+
+<script>
+import Button from './UI/Button.vue';
+import Icon from './UI/Icon.vue';
+
+export default {
+    components: {
+        Button, 
+        Icon
+    }
+}
+</script>
+
+<style>
+.item-frame {
+    height: 4em;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #4444;
+    border-radius: 5px;
+    border: dashed 2px #777;
+}
+</style>
